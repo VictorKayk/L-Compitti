a4ba0e4f565ee242a574d8fa96d31256fea0fdcd
VictorKayk <victorkayk77@outlook.com>
2022-01-14 19:12:54 -0300

    :lipstick: Start the global style

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { render } from "react-dom";
 import { App } from "./App";
 import "./config/i18n";
+import { GlobalStyle } from "./styles/global";
 
 render(
   <StrictMode>
     <App />
+    <GlobalStyle />
   </StrictMode>,
   document.getElementById("root")
 );
---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    width: 375px;
+    height: 600px;
+  }
+`;
