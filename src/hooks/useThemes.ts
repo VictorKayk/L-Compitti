71bca2cf9ee2037db2c9c9e89d9496b87fcd6e27
VictorKayk <victorkayk77@outlook.com>
2022-01-19 19:03:01 -0300

    :recycle: Adding isThemeLoaded to useThemes hook

--- src/hooks/useThemes.ts
+++ src/hooks/useThemes.ts
@@ -7,21 +7,26 @@ import { useConfig } from './useCases';
 export function useThemes(): IUseThemes {
   const { getConfig, setConfig } = useConfig();
   const [currentTheme, setCurrentTheme] = useState(themes.light);
+  const [isThemeLoaded, setIsThemeLoaded] = useState(false);
 
   useEffect(() => {
     async function getTheme(): Promise<void> {
+      setIsThemeLoaded(false);
       const geral = (await getConfig('geral')) as { theme: 'light' | 'dark' };
 
       const theme = geral ? themes[geral.theme] : themes.light;
       setCurrentTheme(theme);
+      setIsThemeLoaded(true);
     }
     getTheme();
   }, []);
 
   async function setTheme(theme: 'light' | 'dark'): Promise<void> {
+    setIsThemeLoaded(false);
     await setConfig('geral', { theme });
     setCurrentTheme(themes[theme]);
+    setIsThemeLoaded(true);
   }
 
-  return { theme: currentTheme, setTheme };
+  return { theme: currentTheme, setTheme, isThemeLoaded };
 }
--- src/interfaces/hooks/IUseThemes.ts
+++ src/interfaces/hooks/IUseThemes.ts
@@ -3,4 +3,5 @@ import { DefaultTheme } from 'styled-components';
 export interface IUseThemes {
   theme: DefaultTheme;
   setTheme: (theme: 'light' | 'dark') => Promise<void>;
+  isThemeLoaded: boolean;
 }
