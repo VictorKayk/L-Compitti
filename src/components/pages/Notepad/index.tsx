d3bcbbd23e4bfbbf783652ea2e5705c8890afaa3
VictorKayk <victorkayk77@outlook.com>
2022-01-25 20:21:04 -0300

    :lipstick: Adding help icon and notepad creating in notepad page

--- src/components/pages/Notepad/index.tsx
+++ src/components/pages/Notepad/index.tsx
@@ -1,9 +1,11 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import * as yup from 'yup';
 import { useNotepad } from '../../../hooks/useCases';
 import { INotepad } from '../../../interfaces/notepad';
 import { ListPage } from '../../ui/templates';
+import { ModalSingleText } from '../../ui/molecules';
 
 export default function Notepad(): ReactElement {
   const { t } = useTranslation();
@@ -13,17 +15,73 @@ export default function Notepad(): ReactElement {
 
   const [notepad, setNotepadsList] = useState([] as INotepad[] | []);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = useCallback(async (): Promise<void> => {
+    setIsModalOpen(true);
+  }, []);
+
+  const closeModal = useCallback(async (): Promise<void> => {
+    setIsModalOpen(false);
+  }, []);
+
   const {
-    notepad: { readAllNotepad },
+    notepad: { readAllNotepad, setNotepad },
   } = useNotepad();
 
-  useEffect(() => {
-    async function getNotepad(): Promise<void> {
-      const allNotepad = await readAllNotepad();
-      setNotepadsList(allNotepad);
-    }
-    getNotepad();
+  const getNotepad = useCallback(async (): Promise<void> => {
+    const allNotepad = await readAllNotepad();
+    setNotepadsList(allNotepad);
   }, [setNotepadsList, readAllNotepad]);
 
-  return <ListPage title={t('notepad')} goBack={goBack} list={notepad} />;
+  const createNotepad = useCallback(
+    async ({ text }): Promise<void> => {
+      await setNotepad({
+        name: text,
+        color: 'theme',
+        notification: null,
+      });
+      closeModal();
+      await getNotepad();
+    },
+    [setNotepad, closeModal, getNotepad]
+  );
+
+  useEffect(() => {
+    getNotepad();
+  }, [getNotepad]);
+
+  const schema = yup
+    .object({
+      text: yup
+        .string()
+        .min(2, t('error-name-notepad-min'))
+        .max(25, t('error-name-notepad-max'))
+        .required(),
+    })
+    .required();
+
+  return (
+    <>
+      <ListPage
+        title={t('notepad')}
+        goBack={goBack}
+        list={notepad}
+        helpActions={[
+          {
+            name: t('create-notepad'),
+            handleClick: openModal,
+          },
+        ]}
+      />
+      <ModalSingleText
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        title={t('create-notepad')}
+        schema={schema}
+        onSubmit={createNotepad}
+        labelTitle={t('notepad-name')}
+        submitButton={t('create')}
+      />
+    </>
+  );
 }
